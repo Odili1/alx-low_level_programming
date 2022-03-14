45c879c30e8ee80ac17382908261261e877a3c2f
Odili1 <ezedigwegerald1@gmail.com>
2022-03-14 07:34:46 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,7 +17,7 @@ n = rand() - RAND_MAX / 2;
 
 if (n == 0)
 {
-priintf("%i is zero\n", n);
+printf("%i is zero\n", n);
 }
 else if (n < 0)
 {
@@ -25,7 +25,7 @@ printf("%i is negative\n", n);
 }
 else
 {
-printf(%i is positive\n", n);
+printf("%i is positive\n", n);
 }
 
 return (0);
