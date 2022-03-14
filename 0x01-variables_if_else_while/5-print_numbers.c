6c37270a4d9ef2193bb9a6fa56fc707d5ac50019
Odili1 <ezedigwegerald1@gmail.com>
2022-03-14 15:19:43 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Entry Point
+ *
+ *Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+int D;
+for(D = '0'; D < 10; D++)
+{
+printf("%D", D);
+}
+
+printf("\n");
+return (0);
+}
