1ea11397c47e0fb9879c4e9f4b7a1bae68b098dd
Odili1 <ezedigwegerald1@gmail.com>
2022-03-14 15:55:20 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (success/correct)
+ */
+int main(void)
+{
+char alphabet;
+ for (alphabet = 12; alphabet >= 97; alphabet--)
+{
+putchar(alphabet);
+}
+putchar('\n');
+return (0);
+}
