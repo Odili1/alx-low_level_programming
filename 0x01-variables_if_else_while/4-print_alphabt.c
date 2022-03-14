48bed2a87d6cebaef0731911d5518f8b80f508e3
Odili1 <ezedigwegerald1@gmail.com>
2022-03-14 14:52:03 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - Entry Point
+ *
+ *Return: Always 0 (Success/correct)
+ */
+int main(void)
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+{
+if (alphabet == 'q')
+continyer;
+
+else if (alphabet == 'e')
+continue;
+
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+}
