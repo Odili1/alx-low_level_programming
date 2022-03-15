d3fab2fb56e96e840e5f5e8067808a0804a53381
Odili1 <ezedigwegerald1@gmail.com>
2022-03-15 16:38:53 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,20 @@
 #include <unistd.h>
 
 /**
- * writes the character c to stdout @c: The character to print
- *
- * Return: On success 1. On error, -1 is returned, and errno is set approximately
+ * main - Entry block
+ *@void: no argument
+ * Return: 0
  */
-
-
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
-}
+	char Odili[] = "_putchar";
 
+	int c;
+
+	for (c = 0; c < 8; c++);
+	{
+		_putchar(Odili[c]);
+	}
+	_putchar('\n');
+	return (0);
+}
