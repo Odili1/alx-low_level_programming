dbeecaef6b316319549f3d7c6e177ca601e6924b
Odili1 <ezedigwegerald1@gmail.com>
2022-03-15 14:47:19 -0700

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *print_alphabet_x10 - repeats the print_alphabet 10 times.
+ *Retrun: Nothing.
+ */
+void print_alphabet_x10(void)
+{
+int i;
+char c;
+
+for (i = 0; i < 10; i++)
+{
+for (c = Ã'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
