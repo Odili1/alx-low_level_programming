188e0ccd1c4e2d04a696929432c273329710ef4f
Odili1 <ezedigwegerald1@gmail.com>
2022-03-15 16:16:44 -0700

    Task 1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,9 +3,12 @@
 /**
  * writes the character c to stdout @c: The character to print
  *
- * Return: Nothing
+ * Return: On success 1. On error, -1 is returned, and errno is set approximately
  */
+
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
