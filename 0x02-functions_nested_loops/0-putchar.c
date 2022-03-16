24fe7b86fc371e7955de1e84d7f6e103d1dfa9af
Odili1 <ezedigwegerald1@gmail.com>
2022-03-15 17:55:09 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-	char Odili[] = "_putchar";
+	char ermias[] = "_putchar";
 
 	int c;
 
 	for (c = 0; c < 8; c++)
 	{
-		_putchar(Odili[c]);
+		_putchar(ermias[c]);
 	}
 	_putchar('\n');
 	return (0);
